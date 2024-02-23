7912ba00d3b9b7e1e58a256521c8a105a85fff01
TannerMarthaler <tannermarthaler@gmail.com>
2024-02-22 20:51:03 -0600

    Created a new service that allows us to track who is currently logged in.

--- NAUIdeaHub/Pages/Authentication.razor.cs
+++ NAUIdeaHub/Pages/Authentication.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using NAUCountryIdeaHub.Services;
 using NAUCountryIdeaHub.Models;
+using NAUIdeaHub.Services;
 
 namespace NAUIdeaHub.Pages
 {
@@ -8,6 +9,11 @@ namespace NAUIdeaHub.Pages
     {
         [Inject] private IIdeaHubService _service { get; set; }
         public IEnumerable<User> users { get; set; } = new List<User>();
+        [Inject] private ILoggedUserService _loggedUser { get; set; }
+        // Service that allows us to share who is logged in to the different pages
+
+        public User loggedInUser;
+        // Value to display the logged in users data
 
         protected override async Task OnInitializedAsync()
         {
@@ -19,6 +25,19 @@ namespace NAUIdeaHub.Pages
             {
                 Console.WriteLine(ex.Message);
             }
+
+        }
+
+        /*
+         * This method will update the LoggedUserService which will allow the other pages to access this data.
+         */
+        public void logIn()
+        {
+            _loggedUser.setUser(users.FirstOrDefault(x => x.Email.Equals(usernameField.Value) && x.Password.Equals(passwordField.Value)));
+            // Sets the shared value in the service to whatever the user inputted
+
+            loggedInUser = _loggedUser.getUser();
+            // Updates the pages user value
         }
 
     }
---
+++ NAUIdeaHub/Services/ILoggedUserService.cs
@@ -0,0 +1,17 @@
+﻿using NAUCountryIdeaHub.Models;
+
+/*
+ * This class is for sharing the variable of who is logged in throughout the project.
+ * This is the interface for the main LoggedUserService class.
+ * This class will outline what is all needed in the LoggedUserService class.
+ */
+
+namespace NAUIdeaHub.Services
+{
+    public interface ILoggedUserService
+    {
+        public User getUser();
+
+        public void setUser(User user);
+    }
+}
