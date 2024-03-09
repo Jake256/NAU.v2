6e22a649120f3d0fafd7fa2b477b883be02c2ef5
brandon-grossman <125304905+brandon-grossman@users.noreply.github.com>
2024-03-09 09:51:20 -0600

    testing email

---
+++ NAUIdeaHub/Services/IMailService.cs
@@ -0,0 +1,9 @@
+﻿using NAUCountryIdeaHub.Models;
+
+namespace NAUIdeaHub.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(string RecipientAddress, string Subject, string HTMLBody);
+    }
+}
---
+++ NAUIdeaHub/Services/MailService.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Mail;
+using System.Net;
+using NAUCountryIdeaHub.Models;
+using NAUIdeaHub.Models;
+
+namespace NAUIdeaHub.Services
+{
+    public class MailService : IMailService
+    {
+        private readonly EmailSettings _emailConfig;
+        public MailService(EmailSettings emailConfig)
+        {
+            _emailConfig = emailConfig;
+        }
+
+        public async Task SendEmailAsync(string RecipientAddress, string Subject, string HTMLBody)
+        {
+            MailMessage message = new MailMessage();
+            SmtpClient smtp = new SmtpClient();
+            message.From = new MailAddress(_emailConfig.SenderAddress);
+            message.To.Add(new MailAddress(RecipientAddress));
+            message.Subject = Subject;
+            message.IsBodyHtml = true;
+            message.Body = HTMLBody;
+            smtp.Port = _emailConfig.Port;
+            smtp.Host = _emailConfig.EmailServerName;
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = true;
+            //smtp.Credentials = new NetworkCredential(_emailConfig.Username, _emailConfig.Password);
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            await smtp.SendMailAsync(message);
+        }
+    }
+}
