5611a47a8ee6e46ba46ba4baa841e172bd55c22c
brandon-grossman <125304905+brandon-grossman@users.noreply.github.com>
2024-03-09 10:34:09 -0600

    Email Service

--- NAUIdeaHub/Models/EmailSettings.cs
+++ NAUIdeaHub/Models/EmailSettings.cs
@@ -2,10 +2,10 @@
 {
     public class EmailSettings
     {
-        public string? Username { get; set; }
-        public string? Password { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
         public int Port { get; set; }
-        public string? SenderAddress { get; set; }
-        public string? EmailServerName { get; set; }
+        public string SenderAddress { get; set; }
+        public string EmailServerName { get; set; }
     }
 }
