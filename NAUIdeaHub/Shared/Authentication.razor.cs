c1f2fe429329393ed8599918da9c29392cc222b0
brandon-grossman <125304905+brandon-grossman@users.noreply.github.com>
2024-03-26 20:22:52 -0500

    Got authentication to do its thing. Recreated the LoggedUser service to support ProtectedSessionStorage.

--- NAUIdeaHub/Shared/Authentication.razor.cs
+++ NAUIdeaHub/Shared/Authentication.razor.cs
@@ -1,15 +1,14 @@
 ﻿using Microsoft.AspNetCore.Components;
 using NAUIdeaHub.Services;
 using NAUIdeaHub.Models;
-using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 
 namespace NAUIdeaHub.Shared
 {
     public partial class Authentication : LayoutComponentBase
     {
         [Inject] private NavigationManager NavigationManager { get; set; }
-        // Allows us to change to a different page within the project.
         [Inject] private IIdeaHubService _service { get; set; }
+        [Inject] private ILoggedUserService protectedSessionStore { get; set; }
         public IEnumerable<User> users { get; set; } = new List<User>();
 
         protected override async Task OnInitializedAsync()
@@ -25,35 +24,20 @@ namespace NAUIdeaHub.Shared
 
         }
 
-        /*
-         * This method will update the LoggedUserService which will allow the other pages to access this data.
-         */
-        //public void updateCurrentUser()
-        //{
-        //    _loggedUser.setUser(users.FirstOrDefault(x => x.Email.Equals(usernameField.Value) && x.Password.Equals(passwordField.Value)));
-        //    // Sets the shared value in the service to whatever the user inputted
-
-        //    loggedInUser = _loggedUser.getUser();
-        //    // Updates the pages user value
-
-        //    //refreshes page
-        //    NavigationManager.NavigateTo(NavigationManager.Uri, true);
-
-        //}
-
         public async void SetAuthenticatedUser(User user)
         {
-            await SetSessionAuthenticatedUser();
+            await SetSessionAuthenticatedUser(user);
 
             //refreshes page
             NavigationManager.NavigateTo(NavigationManager.Uri, true);
 
         }
 
-        private async Task SetSessionAuthenticatedUser()
+        private async Task SetSessionAuthenticatedUser(User user)
         {
-            
-            await ProtectedSessionStore.SetAsync("AuthenticatedUser", users.FirstOrDefault(x => x.Email.Equals(usernameField.Value) && x.Password.Equals(passwordField.Value)));
+            User loggedInUser = users.FirstOrDefault(user);
+            protectedSessionStore.SetUser(loggedInUser);
+            await ProtectedSessionStore.SetAsync("AuthenticatedUser", loggedInUser);
         }
 
     }
