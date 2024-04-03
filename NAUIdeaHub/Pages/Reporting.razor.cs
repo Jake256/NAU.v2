515d1026ce81185d3d397886da4b4692f987315b
brandon-grossman <125304905+brandon-grossman@users.noreply.github.com>
2024-04-02 21:23:00 -0500

    Reporting page

--- NAUIdeaHub/Pages/Reporting.razor.cs
+++ NAUIdeaHub/Pages/Reporting.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using NAUIdeaHub.Models;
 using NAUIdeaHub.Services;
+using static MudBlazor.Colors;
 
 namespace NAUIdeaHub.Pages
 {
@@ -11,13 +12,28 @@ namespace NAUIdeaHub.Pages
         [Inject] private ILoggedUserService protectedSessionStore { get; set; }
 
         public User? authenticatedUser;
+        public string[]? userFullNames;
         public IEnumerable<Request> requests { get; set; } = new List<Request>();
+        public List<Request> openRequests { get; set; } = new List<Request>();
+        public List<Request> closedRequests { get; set; } = new List<Request>();
 
         protected override async Task OnInitializedAsync()
         {
             authenticatedUser = await protectedSessionStore.GetUserAsync();
 
             requests = await _service.GetRequestsByUserAsync(authenticatedUser.UserID);
+
+            foreach (var request in requests)
+            {
+                if (request.Closed)
+                {
+                    closedRequests.Add(request);
+                }
+                else
+                {
+                    openRequests.Add(request);
+                }
+            }
 
         }
 
